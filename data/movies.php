f70c0f5df697ae66f7a2c2e63511a6cd3fe08938
Matthew Setter <matthew@matthewsetter.com>
2020-04-02 11:36:23 +0200

    First iteration of the manual build

---
+++ data/movies.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    [
+        'title' => 'Hot Fuzz',
+        'director' => 'Edgar Wright',
+        'release_date' => '2007/02/14',
+        'synopsis' => 'Exceptional London cop Nicholas Angel is involuntarily transferred to a quaint English village and paired with a witless new partner. While on the beat, Nicholas suspects a sinister conspiracy is afoot with the residents.',
+        'stars' => [
+            'Martin Freeman',
+            'Nick Frost',
+            'Simon Pegg',
+        ],
+        'genre' => [
+            'action',
+            'comedy',
+            'mystery',
+        ]
+    ]
+];
